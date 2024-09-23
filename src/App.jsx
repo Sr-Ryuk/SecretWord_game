6e81ec12c4306bd65b86d1fe3aa0e5455430d0e1
Diogo Wallace <diogo.wallaceferreira@gmail.com>
2024-09-23 19:22:33 -0300

    criação de states do game, importação dos dados e componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+//CSS
 import './App.css'
+
+//REACT IMPORTS
+import { useCallback, useEffect, useState } from 'react'
+
+//DATA
+import { wordsList } from './data/word'
+
+//COMPONENTS
 import StartScreen from './components/StartScreen'
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  {id:1, name:'start'},
+  {id:2, name:'game'},
+  {id:3, name:'end'}
+];
 
 function App() {
 
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList);
+
+  console.log(words);
+
   return (
     <>
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen/>}
+      {gameStage === 'game' && <Game/>}
+      {gameStage === 'end' && <GameOver/>}
     </>
   )
 }
