ac7ab7c85533d6458f9ba0a35887474fc8e6d1b9
Diogo Wallace <diogo.wallaceferreira@gmail.com>
2024-09-25 00:43:42 -0300

    Adicionando tratamento de props

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,12 +1,18 @@
 import './GameOver.css';
+import PropTypes from 'prop-types';
 
 
-const GameOver = () => {
+const GameOver = ({ retry }) => {
   return (
     <div>
       <h2>Game Over</h2>
+      <button onClick={retry} >Resetar jogo</button>
     </div>
   );
+};
+
+GameOver.propTypes = {
+  retry: PropTypes.func.isRequired, // exemplo para uma prop do tipo função
 };
 
 export default GameOver;
